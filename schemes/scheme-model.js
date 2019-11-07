8a9c854450e773edff1ec4f9ced217cd81682712
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-11-07 08:51:43 +0000

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,11 +1,40 @@
 const db = require("../data/db-config");
-module.exports  = {
+module.exports = {
     find,
-    // findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+  };
+  
+  function remove(id) {
+    return db("schemes as s")
+      .where({ id })
+      .delete();
+  }
+  
+  function update(changes, id) {
+    return db("schemes as s")
+      .where({ id })
+      .update(changes);
+  }
+  
+  function add(scheme) {
+    return db("schemes as s")
+      .insert(scheme)
+      .then(ids => findById(ids[0]));
+  }
+  
+  function findSteps(id) {
+    return db("steps as st")
+      .join("schemes as s", "st.scheme_id", "s.id")
+      .select("s.id", "s.scheme_name", "st.step_number")
+      .where({ scheme_id: id });
+  }
+  
+  function findById(schemesId) {
+    return db("schemes as s").where({ id: schemesId });
 }
 
 function find() {
